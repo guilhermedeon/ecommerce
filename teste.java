56c74eef500ad3d6867578ca0447d6c187d291b8
Guilherme Deon de Faria <guibyn@gmail.com>
2023-06-14 21:06:06 -0300

    Delete teste.java

--- teste.java
+++
@@ -1,8 +0,0 @@
-public class teste {
-    public static void main(String args[]){
-        Long long1 = 1L;
-        Double a = Double.valueOf(long1);
-        Math.min()
-        System.out.println(long1+a);
-    }
-}
